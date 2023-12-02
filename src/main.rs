49c4de06964ceb3a3418e558763f1ffd8c728a1d
Raoul1808 <raoulthegeek@gmail.com>
2023-12-02 14:12:40 +0100

    Display list of checkboxes

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,32 @@
-use iced::{widget::text, Sandbox, Settings};
+use iced::{
+    widget::{checkbox, column},
+    Element, Sandbox, Settings,
+};
 
 fn main() -> iced::Result {
     ModManager::run(Settings::default())
 }
 
-struct ModManager;
+struct ModManager {
+    mod_list: Vec<(bool, String)>,
+}
 
 #[derive(Debug, Clone)]
-enum Message {}
+enum Message {
+    Toggle(usize, bool)
+}
 
 impl Sandbox for ModManager {
     type Message = Message;
 
     fn new() -> Self {
-        Self
+        let mut mods = Vec::new();
+        for i in 0..10 {
+            mods.push((rand::random::<i32>() % 2 == 1, format!("Testing {i}")));
+        }
+        Self {
+            mod_list: mods,
+        }
     }
 
     fn title(&self) -> String {
@@ -21,10 +34,21 @@ impl Sandbox for ModManager {
     }
 
     fn update(&mut self, message: Message) {
-        match message {}
+        match message {
+            Message::Toggle(i, b) => self.mod_list.get_mut(i).and_then(|m| Some(m.0 = b)),
+        };
     }
 
-    fn view(&self) -> iced::Element<'_, Message> {
-        text("Hello mod manager with iced!").into()
+    fn view(&self) -> Element<'_, Message> {
+        column(
+            self.mod_list
+                .iter()
+                .enumerate()
+                .map(|(i, (e, m))| checkbox(m, *e, move |b| Message::Toggle(i, b)).into())
+                .collect(),
+        )
+        .spacing(10)
+        .padding(20)
+        .into()
     }
 }
