57799ee3e440f1d856f2b5def4743968321d3859
Raoul1808 <raoulthegeek@gmail.com>
2023-12-02 12:53:18 +0100

    Create iced app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use iced::{widget::text, Sandbox, Settings};
+
+fn main() -> iced::Result {
+    ModManager::run(Settings::default())
+}
+
+struct ModManager;
+
+#[derive(Debug, Clone)]
+enum Message {}
+
+impl Sandbox for ModManager {
+    type Message = Message;
+
+    fn new() -> Self {
+        Self
+    }
+
+    fn title(&self) -> String {
+        String::from("Icy Isaac Mod Manager")
+    }
+
+    fn update(&mut self, message: Message) {
+        match message {}
+    }
+
+    fn view(&self) -> iced::Element<'_, Message> {
+        text("Hello mod manager with iced!").into()
+    }
 }
