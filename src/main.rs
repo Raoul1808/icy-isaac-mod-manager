a260ab5aaff3ab6b760c5dda9ccca6ca8f0c4340
Raoul1808 <raoulthegeek@gmail.com>
2023-12-02 17:34:14 +0100

    Integrate with local mods

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
+use std::{
+    fs::{self, File},
+    io,
+    path::{Path, PathBuf},
+};
+
 use iced::{
-    widget::{checkbox, column},
+    widget::{checkbox, column, container, scrollable},
     Element, Sandbox, Settings,
 };
 
@@ -7,8 +13,35 @@ fn main() -> iced::Result {
     ModManager::run(Settings::default())
 }
 
+fn create_empty_file(path: PathBuf) -> io::Result<()> {
+    let _ = File::create(path)?;
+    Ok(())
+}
+
+struct Mod {
+    name: String,
+    path: PathBuf,
+}
+
+impl Mod {
+    pub fn disable_path(&self) -> PathBuf {
+        self.path.join("disable.it")
+    }
+
+    pub fn enabled(&self) -> bool {
+        !self.disable_path().exists()
+    }
+
+    pub fn set_enabled(&mut self, enabled: bool) -> io::Result<()> {
+        match enabled {
+            true => fs::remove_file(self.disable_path()),
+            false => create_empty_file(self.disable_path()),
+        }
+    }
+}
+
 struct ModManager {
-    mod_list: Vec<(bool, String)>,
+    mod_list: Vec<Mod>,
 }
 
 #[derive(Debug, Clone)]
@@ -20,9 +53,21 @@ impl Sandbox for ModManager {
     type Message = Message;
 
     fn new() -> Self {
+        const MOD_PATH: &str =
+            "/ssd/SteamLibrary/steamapps/common/The Binding of Isaac Rebirth/mods/";
         let mut mods = Vec::new();
-        for i in 0..10 {
-            mods.push((rand::random::<i32>() % 2 == 1, format!("Testing {i}")));
+        if let Ok(dir) = fs::read_dir(Path::new(MOD_PATH)) {
+            for entry in dir.flatten() {
+                let path = entry.path();
+                if path.is_dir() {
+                    let p = String::from(
+                        path.file_name()
+                            .unwrap_or(path.as_os_str())
+                            .to_string_lossy(),
+                    );
+                    mods.push(Mod { name: p, path });
+                }
+            }
         }
         Self { mod_list: mods }
     }
@@ -33,20 +78,26 @@ impl Sandbox for ModManager {
 
     fn update(&mut self, message: Message) {
         match message {
-            Message::Toggle(i, b) => self.mod_list.get_mut(i).map(|m| m.0 = b),
+            Message::Toggle(i, b) => self.mod_list.get_mut(i).map(|m| m.set_enabled(b)),
         };
     }
 
     fn view(&self) -> Element<'_, Message> {
-        column(
+        let mod_list = column(
             self.mod_list
                 .iter()
                 .enumerate()
-                .map(|(i, (e, m))| checkbox(m, *e, move |b| Message::Toggle(i, b)).into())
+                .map(|(i, m)| {
+                    checkbox(m.name.to_owned(), m.enabled(), move |b| {
+                        Message::Toggle(i, b)
+                    })
+                    .into()
+                })
                 .collect(),
         )
         .spacing(10)
-        .padding(20)
-        .into()
+        .padding(20);
+        let scroll = scrollable(mod_list);
+        container(scroll).padding(20).into()
     }
 }
